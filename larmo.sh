6a8ab3598fd83c015de9663746aeb881b0de075a
Adrian Pietka <adrian@pietka.me>
2015-12-24 00:12:31 +0100

    Refactoring installation script

--- larmo.sh
+++ larmo.sh
@@ -1,36 +1,51 @@
 #!/usr/bin/env bash
 
+# variables
+
 DIRECTORY=$(pwd)"/"
-DIRECTORY_SERVER=$DIRECTORY"larmo/server"
-DIRECTORY_WEBAPP=$DIRECTORY"larmo/webapp"
-DIRECTORY_WEBHOOKS=$DIRECTORY"larmo/webhooks"
+
+SERVER_DIRECTORY=$DIRECTORY"larmo/server"
+SERVER_REPOSITORY="git@github.com:larmo-hub/larmo-server.git"
+
+WEBAPP_DIRECTORY=$DIRECTORY"larmo/webapp"
+WEBAPP_REPOSITORY="git@github.com:larmo-hub/larmo-webapp.git"
+
+WEBHOOKS_DIRECTORY=$DIRECTORY"larmo/webhooks"
+WEBHOOKS_REPOSITORY="git@github.com:larmo-hub/larmo-webhooks-agent.git"
+
+# check required software
 
 git --version >/dev/null 2>&1 || { echo >&2 "I require 'git' but it's not installed.  Aborting."; exit 1; }
 docker -v >/dev/null 2>&1 || { echo >&2 "I require 'docker' but it's not installed.  Aborting."; exit 1; }
 docker-compose -v >/dev/null 2>&1 || { echo >&2 "I require 'docker-compose' but it's not installed.  Aborting."; exit 1; }
 
+# functions
+
+function updateSourceCode {
+    TARGET_DIRECTORY=$1
+    GIT_REPOSITORY_URL=$2
+    
+    if [ -d $TARGET_DIRECTORY ]; then
+        cd $TARGET_DIRECTORY && git pull origin master && cd $DIRECTORY
+    else
+        git clone $GIT_REPOSITORY_URL $TARGET_DIRECTORY
+    fi
+}
+
+# create root directory
+
 if [ ! -d $DIRECTORY ]; then
     mkdir $DIRECTORY
 fi
 
-if [ -d $DIRECTORY_SERVER ]; then
-    cd $DIRECTORY_SERVER && git pull origin master && cd $DIRECTORY
-else
-    git clone git@github.com:larmo-hub/larmo-server.git $DIRECTORY_SERVER
-fi
+# update source code
 
-if [ -d $DIRECTORY_WEBAPP ]; then
-    cd $DIRECTORY_WEBAPP && git pull origin master && cd $DIRECTORY
-else
-    git clone git@github.com:larmo-hub/larmo-webapp.git $DIRECTORY_WEBAPP
-fi
+updateSourceCode $SERVER_DIRECTORY $SERVER_REPOSITORY
+updateSourceCode $WEBAPP_DIRECTORY $WEBAPP_REPOSITORY
+updateSourceCode $WEBHOOKS_DIRECTORY $WEBHOOKS_REPOSITORY
 
-if [ -d $DIRECTORY_WEBHOOKS ]; then
-    cd $DIRECTORY_WEBHOOKS && git pull origin master && cd $DIRECTORY
-else
-    git clone git@github.com:larmo-hub/larmo-webhooks-agent.git $DIRECTORY_WEBHOOKS
-fi
+# run containers
 
-cd $DIRECTORY_SERVER && docker-compose up
-cd $DIRECTORY_WEBAPP && docker-compose up
-cd $DIRECTORY_WEBHOOKS && docker-compose up
+cd $SERVER_DIRECTORY && docker-compose up
+cd $WEBAPP_DIRECTORY && docker-compose up
+cd $WEBHOOKS_DIRECTORY && docker-compose up
